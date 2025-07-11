3172548553e9e84f220be6a1c06112764dd5b91d
kyoulau <laucosan05@gmail.com>
2025-07-10 21:06:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {  BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route  element={<CreateRoom />} index/>
+          <Route element={<Room />} path="/room" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+    
+  )
+}
